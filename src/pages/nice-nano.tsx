c4a66abdafd6d574e5fdd4c0c2cc56665fb8a769
Nick Winans <nick@winans.codes>
2021-03-04 20:22:15 -0600

    Add thin video

--- src/pages/nice-nano.tsx
+++ src/pages/nice-nano.tsx
@@ -1,4 +1,13 @@
-import { Box, useColorModeValue, Container, Flex, chakra, Link, Button, Text } from "@chakra-ui/react";
+import {
+  Box,
+  useColorModeValue,
+  Container,
+  Flex,
+  chakra,
+  Link,
+  Button,
+  Text,
+} from "@chakra-ui/react";
 import { faArrowRight } from "@fortawesome/pro-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { StaticImage } from "gatsby-plugin-image";
@@ -17,64 +26,135 @@ const niceNano = () => {
         />
         <html lang="en" />
       </Helmet>
-          <Box mb={20}>
-      <Box as="section" pt={{ base: "6rem", md: "8rem" }} pb="3rem" backgroundImage={`url('/nicenanobg-${useColorModeValue('light', 'dark')}.svg')`} backgroundSize="cover" backgroundPosition="center">
-        <Container maxW="1280px" my="2rem">
-          <Flex justify="center" wrap="wrap">
-            <Box
-              mr="1em"
-              maxHeight="50vh"
-              maxWidth="275px"
-              w="100%"
-              ml="1rem"
-            >
-              <StaticImage
-                src="../images/nicenano.png"
-                alt="nice!nano"
-                height={400}
-                placeholder="none"
-                imgStyle={{ objectFit: 'contain', maxHeight: '50vh' }}
-                loading="eager"
-                draggable={false} 
-                formats={['auto', 'webp', 'avif']}
-                quality={80}
-              />
-            </Box>
-            <Box textAlign="center" pt="2rem">
-              <chakra.h1
-                maxW="20ch"
-                mx="auto"
-                fontSize={{ base: "2.25rem", sm: "3rem", lg: "4rem" }}
-                fontFamily="heading"
-                letterSpacing="tight"               
-                fontWeight="bold"
-                mb="16px"
-                lineHeight="1.2"
+      <Box mb={20}>
+        <Box
+          as="section"
+          pt={{ base: "6rem", md: "8rem" }}
+          pb="3rem"
+          backgroundImage={`url('/nicenanobg-${useColorModeValue(
+            "light",
+            "dark"
+          )}.svg')`}
+          backgroundSize="cover"
+          backgroundPosition="center"
+        >
+          <Container maxW="1280px" my="2rem">
+            <Flex justify="center" wrap="wrap">
+              <Box
+                mr="1em"
+                maxHeight="50vh"
+                maxWidth="275px"
+                w="100%"
+                ml="1rem"
               >
-                Meet the
-                <Box
-                  as="span"
-                  color={useColorModeValue("cyan.500", "cyan.300")}
+                <StaticImage
+                  src="../images/nicenano.png"
+                  alt="nice!nano"
+                  height={400}
+                  placeholder="none"
+                  imgStyle={{ objectFit: "contain", maxHeight: "50vh" }}
+                  loading="eager"
+                  draggable={false}
+                  formats={["auto", "webp", "avif"]}
+                  quality={80}
+                />
+              </Box>
+              <Box textAlign="center" pt="2rem">
+                <chakra.h1
+                  maxW="20ch"
+                  mx="auto"
+                  fontSize={{ base: "2.25rem", sm: "3rem", lg: "4rem" }}
+                  fontFamily="heading"
+                  letterSpacing="tight"
+                  fontWeight="bold"
+                  mb="16px"
+                  lineHeight="1.2"
                 >
-                  {" "}
-                  nice!nano
-                </Box>
-              </chakra.h1>
+                  Meet the
+                  <Box
+                    as="span"
+                    color={useColorModeValue("cyan.500", "cyan.300")}
+                  >
+                    {" "}
+                    nice!nano
+                  </Box>
+                  .
+                </chakra.h1>
 
-              <Text
-                maxW="560px"
-                mx="auto"
-                opacity={0.7}
-                fontSize={{ base: "lg", lg: "xl" }}
-                mt="6"
+                <Text
+                  maxW="560px"
+                  mx="auto"
+                  opacity={0.7}
+                  fontSize={{ base: "lg", lg: "xl" }}
+                  mt="6"
+                >
+                  The nice!nano is a Pro Micro drop-in replacement board that
+                  makes building wireless keyboards a breeze.
+                </Text>
+              </Box>
+            </Flex>
+          </Container>
+        </Box>
+        <Box as="section" pt={{ base: "2rem", md: "4rem" }} pb="3rem">
+          <Container maxW="1280px" my="2rem">
+            <Flex justify="center" alignItems="center" wrap="wrap">
+              <Box
+                mr="1em"
+                maxHeight="50vh"
+                maxWidth="275px"
+                w="100%"
+                ml="1rem"
               >
-                The nice!nano is a Pro Micro drop-in replacement board that
-                makes building wireless keyboards a breeze.
-              </Text>
-            </Box>
-          </Flex>
-        </Container>
-      </Box>
+                <video
+                  height={450}
+                  style={{ maxHeight: "50vh", height: "450px" }}
+                  autoPlay
+                  loop
+                  muted
+                >
+                  <source src="/nicenano-thin.webm" type="video/webm" />
+                  <source
+                    src="/nicenano-thin.m4v"
+                    type="video/quicktime"
+                  ></source>
+                </video>
+              </Box>
+              <Box textAlign="center" pt="2rem">
+                <chakra.h1
+                  maxW="20ch"
+                  mx="auto"
+                  fontSize={{ base: "2.25rem", sm: "3rem", lg: "4rem" }}
+                  fontFamily="heading"
+                  letterSpacing="tight"
+                  fontWeight="bold"
+                  mb="16px"
+                  lineHeight="1.2"
+                >
+                  Super
+                  <Box
+                    as="span"
+                    color={useColorModeValue("cyan.500", "cyan.300")}
+                  >
+                    {" "}
+                    Thin
+                  </Box>
+                  .
+                </chakra.h1>
+
+                <Text
+                  maxW="560px"
+                  mx="auto"
+                  opacity={0.7}
+                  fontSize={{ base: "lg", lg: "xl" }}
+                  mt="6"
+                >
+                  The nice!nano uses a mid-mount USB-C port making it only 3.2mm
+                  thick. That's thinner than a Pro Micro.
+                </Text>
+              </Box>
+            </Flex>
+          </Container>
+        </Box>
       </Box>
     </Layout>
   );
