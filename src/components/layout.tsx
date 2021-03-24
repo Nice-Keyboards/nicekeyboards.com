35ab1cc7ebb45e145fbd4f979b4c885ba07d7173
Nick Winans <nick@winans.codes>
2021-03-24 01:35:40 -0500

    fix2

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -15,14 +15,16 @@ export default function Layout({ children }) {
     >
       <Helmet>
         <html lang="en" />
-        <script type="text/javascript">
-          {`
+      </Helmet>
+      <Header />
+      <script type="text/javascript">
+        {`
             _atrk_opts = { atrk_acct:"VLImw1WyR620WR", domain:"nicekeyboards.com",dynamic: true};
             (function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://certify-js.alexametrics.com/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
           `}
-        </script>
-        <noscript>
-          {`
+      </script>
+      <noscript>
+        {`
             <img
               src="https://certify.alexametrics.com/atrk.gif?account=VLImw1WyR620WR"
               style={{ display: "none" }}
@@ -31,9 +33,7 @@ export default function Layout({ children }) {
               alt=""
             />
           `}
-        </noscript>
-      </Helmet>
-      <Header />
+      </noscript>
       {children}
       <Footer />
     </div>
