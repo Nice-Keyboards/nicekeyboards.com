235d862b4e42cc3edf0704d8f128aa3dcdf09507
Nick Winans <nick@winans.codes>
2022-01-07 19:36:18 -0600

    Add Stock Checker

--- src/components/find-a-store.tsx
+++ src/components/find-a-store.tsx
@@ -1,10 +1,22 @@
-import { Box, chakra, Flex, Heading, Link, useColorModeValue } from "@chakra-ui/react";
-import React from "react";
+import { Box, chakra, Flex, Heading, Link, Tooltip, useColorModeValue } from "@chakra-ui/react";
+import React, { useEffect, useRef, useState } from "react";
 import Flags from "country-flag-icons/react/3x2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle, faQuestionCircle, faTimesCircle } from "@fortawesome/pro-duotone-svg-icons";
 
 interface StoreRegion {
   region: string;
-  stores: { name: string; url: string; country: string}[];
+  stores: {
+    name: string;
+    url: string;
+    country: string;
+    type: string;
+    stock: {
+      tag: string;
+      inStock: string;
+      outOfStock: string;
+    };
+  }[];
 }
 
 const shuffleArray = (arr: Array<any>): Array<any> => {
@@ -17,8 +29,135 @@ const shuffleArray = (arr: Array<any>): Array<any> => {
 };
 
 export default function FindAStore({stores}: {stores: StoreRegion[]}) {
+  const [storesState, setStoresState] = useState(stores);
+  const [stock, setStock] = useState({});
+  const [checkedStock, setCheckedStock] = useState(false);
+  const container = useRef(null);
+
+  useEffect(() => {
+    let regions = [...storesState];
+
+    regions.forEach(r => {
+      r.stores = shuffleArray(r.stores);
+    });
+
+    setStoresState(regions);
+  }, []);
+
+  const checkStock = async () => {
+    let stockCopy = {...stock};
+
+    await Promise.all(storesState.map(async (r) => await Promise.all(r.stores.map(async (s) => {
+      try {
+        switch (s.type) {
+          case 'shopify':
+            const json = await (await fetch(s.url + ".js")).json();
+            stockCopy[s.url] = json.available;
+            break;
+          case 'klaviyo':
+            const json2 = await (await fetch(s.stock.link)).json();
+            stockCopy[s.url] = json2.data.variants[0].inventory_quantity > 0;
+            break;
+          case 'squarespace':
+            if (!s.stock) {
+              s.stock = {
+                tag: ".product-mark.sold-out",
+                inStock: undefined,
+                outOfStock: "sold out",
+              };
+            }
+          case 'woocommerce':
+            if (!s.stock) {
+              s.stock = {
+                tag: ".single_add_to_cart_button",
+                inStock: "Add to cart",
+                outOfStock: undefined,
+              };
+            }
+          case 'custom':
+            const res = await fetch("https://cors.nicell.workers.dev/?" + s.url);
+            if (res.status >= 400) {
+              break;
+            }
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(
+              await res.text(),
+              "text/html"
+            );
+            const stockText = doc.querySelector(s.stock.tag)?.textContent;
+            if (stockText === s.stock.inStock)  {
+              stockCopy[s.url] = true;
+            } else if (stockText === s.stock.outOfStock) {
+              stockCopy[s.url] = false;
+            }
+            break;
+          default:
+            stockCopy[s.url] = undefined;
+        }
+        setStock({...stockCopy});
+      } catch {
+        stockCopy[s.url] = undefined;
+      }
+    }))));
+
+    setStock({...stockCopy});
+    
+  }
+
+  const stockIcon = (available: boolean) => {
+    switch (available) {
+      case true:
+        return (
+          <Tooltip label="In stock" openDelay={300}>
+            <Box pl="0.25rem">
+              <FontAwesomeIcon color="#4caf50" icon={faCheckCircle} />
+            </Box>
+          </Tooltip>
+        );
+      case false:
+        return (
+          <Tooltip label="Out of stock" openDelay={300}>
+            <Box pl="0.25rem">
+              <FontAwesomeIcon color="#f44336" icon={faTimesCircle} />
+            </Box>
+          </Tooltip>
+        );
+      default:
+        return (
+          <Tooltip label="Stock Unknown" openDelay={300}>
+            <Box pl="0.25rem">
+              <FontAwesomeIcon color="#ffb300" icon={faQuestionCircle} />
+            </Box>
+          </Tooltip>
+        );
+    }
+  }
+
+  const callbackFunction = (entries) => {
+    const [entry] = entries;
+    if (entry.isIntersecting && !checkedStock) {
+      setCheckedStock(true);
+      checkStock();
+    }
+  };
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.01,
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(callbackFunction, options);
+    if (container.current) observer.observe(container.current);
+
+    return () => {
+      if (container.current) observer.unobserve(container.current);
+    };
+  }, [container, options]);
+
   return (
-    <Box textAlign="center" pt="3rem" id="find-a-store">
+    <Box textAlign="center" pt="3rem" id="find-a-store" ref={container}>
       <chakra.h1
         maxW="20ch"
         mx="auto"
@@ -57,8 +196,8 @@ export default function FindAStore({stores}: {stores: StoreRegion[]}) {
           >
             {region.region}
           </Heading>
-          <Flex justify="center" alignItems="center" wrap="wrap" maxW="460px">
-            {shuffleArray(region.stores).map((store) => {
+          <Flex justify="center" alignItems="center" wrap="wrap" maxW="500px">
+            {region.stores.map((store) => {
               const Flag = Flags[store.country];
               return (
                 <Link
@@ -76,6 +215,7 @@ export default function FindAStore({stores}: {stores: StoreRegion[]}) {
                 >
                   <Flag width="2rem" style={{ marginRight: "0.25rem" }} />
                   {store.name}
+                  {stockIcon(stock[store.url])}
                 </Link>
               );
             })}
